10788600ebaa54f0239a086a3f9745e77604ca3b
yindongsheng <yindongsheng@mail.rmd-safe.gov.cn>
2023-05-29 01:53:26 +0800

    feat: add yarn.lock and package-lock.json

--- stories/Page.stories.ts
+++ stories/Page.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import { within, userEvent } from '@storybook/testing-library';
+// import { within, userEvent } from '@storybook/testing-library';
 import MyPage from './Page.vue';
 
 const meta = {
@@ -21,14 +21,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on interaction testing: https://storybook.js.org/docs/vue/writing-tests/interaction-testing
-export const LoggedIn: Story = {
-  play: async ({ canvasElement }: any) => {
-    const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
-    await userEvent.click(loginButton);
-  },
-};
+// export const LoggedIn: Story = {
+//   play: async ({ canvasElement }: any) => {
+//     const canvas = within(canvasElement);
+//     const loginButton = await canvas.getByRole('button', {
+//       name: /Log in/i,
+//     });
+//     await userEvent.click(loginButton);
+//   },
+// };
 
 export const LoggedOut: Story = {};
